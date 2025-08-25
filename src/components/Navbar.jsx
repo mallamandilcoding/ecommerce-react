9f133c9c9d097d1cbf7b02756e0d7e4609790c76
mandil malla <mallamandilcoding@gmail.com>
2025-08-25 22:20:27 +0900

    navbar header done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <nav className="bg-white shadow-md ">
+        <div className="flex items-center p-4">
+          <div className="text-lg font-bold">
+            <Link to="/">e-SHOP</Link>
+          </div>
+          <div className="relative flex-1 mx-4">
+            <form action="">
+              <input
+                type="text"
+                placeholder="Search Product"
+                className="w-full border py-2 px-4"
+              />
+              <FaSearch className="absolute top-3 right-3 text-red-500" />
+            </form>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Link to="/cart">
+              <FaShoppingCart className="text-lg" />
+            </Link>
+            <button className="hidden md:block">Login | Register</button>
+            <button className="block md:hidden">
+              <FaUser />
+            </button>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
